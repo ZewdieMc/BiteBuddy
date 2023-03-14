99035d63245654841635c6579a9513470095e15b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-14 23:59:45 +0100

    Add comments popup

--- src/modules/populateMeals.js
+++ src/modules/populateMeals.js
@@ -1,12 +1,15 @@
-import getMeals, { meals } from './meals.js';
+import getMeals from './meals.js';
 
 const populateMeals = () => {
-  console.log(meals);
   const mealsContainer = document.querySelector('.mealsContainer');
   getMeals().then((data) => {
     data.categories.slice(0, 9).forEach((meal) => {
       const mealDiv = document.createElement('div');
       mealDiv.classList.add('meal');
+
+      const modal = document.createElement('section');
+      modal.classList.add('hide', 'modalPopup');
+      modal.setAttribute('id', `${meal.idCategory}`);
       mealDiv.innerHTML = `
       <div class="meal-img">
         <img src="${meal.strCategoryThumb}" alt="${meal.strCategory}">
@@ -17,6 +20,20 @@ const populateMeals = () => {
       </div>
       <button data-mealId = '${meal.idCategory}'class="comment-btn">Comments</button>
     `;
+
+      modal.innerHTML = `
+      <div class="modal-container">
+      <div class="closeIcon">
+       <i class="fa-sharp fa-solid fa-xmark closeModal"></i>
+     </div>
+     <div class="mealContent">
+      <img src="${meal.strCategoryThumb}" alt="${meal.strCategory} image">
+       <h2>${meal.strCategory}</h2>
+       <p>${meal.strCategoryDescription}</p>        
+     </div>
+     </div>
+      `;
+      mealDiv.appendChild(modal);
       mealsContainer.appendChild(mealDiv);
     });
   });
