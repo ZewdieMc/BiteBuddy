bfcf549f224309972488b6e92cf9d800e91a2c4c
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-14 23:43:29 +0300

    Import logo Icon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import './style.css';
-
+import './style.scss';
+import logo from './assets/images/bitebuddy.png';
 import populateMeals from './modules/populateMeals.js';
 
+const logoContainer = document.getElementById('bitebuddy-logo');
+logoContainer.src = logo;
 populateMeals();
