6ed35b85fb9ff14a09dd02e1b1b17b433eb0ea8d
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-15 00:00:40 +0100

    Add event listeners for comment and close popup buttons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import './style.css';
 import populateMeals from './modules/populateMeals.js';
 
 populateMeals();
+
+window.onload = () => {
+  const commentBtn = document.querySelectorAll('.comment-btn');
+  commentBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      document.getElementById(`${index + 1}`).classList.toggle('hide');
+    });
+  });
+
+  const close = document.querySelectorAll('.closeModal');
+  close.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      document.getElementById(`${index + 1}`).classList.toggle('hide');
+    });
+  });
+};
