ca5e3ad6604b0345722bd956e03255dd1f96b928
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-15 00:01:38 +0100

    Export default getMeals

--- src/modules/meals.js
+++ src/modules/meals.js
@@ -1,10 +1,8 @@
 const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-// eslint-disable-next-line import/no-mutable-exports
-let meals = [];
+
 const getMeals = async () => {
   const response = await fetch(url);
   const data = await response.json();
-  meals = data.categories;
   return data;
 };
-export { getMeals, meals };
+export default getMeals;
