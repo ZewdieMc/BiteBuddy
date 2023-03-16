3862b5ce6feef1ea3e1d2b68790a65a74fdbc10d
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-16 15:30:07 +0300

    Add like to the items

--- src/modules/populateMeals.js
+++ src/modules/populateMeals.js
@@ -1,4 +1,5 @@
 import pushComment from './involvement.js';
+import Like from './like.js';
 import fetchData from './meals.js';
 
 const displayLikes = (likes) => {
@@ -6,10 +7,23 @@ const displayLikes = (likes) => {
     document.getElementById(`${like.item_id}`).textContent = `${like.likes}`;
   });
 };
+
+const handleLike = (elements) => {
+  elements.forEach((element) => {
+    element.addEventListener('click', async (e) => {
+      const status = await Like.postLike(e.target.dataset.itemid);
+      if (status === 201) {
+        Like.getLikes().then((likes) => {
+          displayLikes(likes);
+        });
+      }
+    });
+  });
+};
+
 const populateMeals = async () => {
   const mealsContainer = document.querySelector('.mealsContainer');
   const [meals, likes] = await fetchData();
-  // meals.then((data) => {
   meals.categories.slice(0, 9).forEach((meal) => {
     const mealDiv = document.createElement('div');
     mealDiv.classList.add('meal');
@@ -25,7 +39,7 @@ const populateMeals = async () => {
       <div class="meal-stats">
         <h3>${meal.strCategory}</h3>
         <div>
-          <i class="fa-regular fa-heart fa-2x"></i><p> <span id='item-${meal.idCategory}'class="badge bg-danger rounded-pill px-3 ">0</span> Likes </p></div>
+          <i data-itemid = 'item-${meal.idCategory}'class="fa-regular fa-heart fa-2x like"></i><p> <span id='item-${meal.idCategory}'class="badge bg-danger rounded-pill px-3 ">0</span> Likes </p></div>
       </div>
       <button data-mealId = '${meal.idCategory}'class="btn btn-success comment-btn">Comments</button>
       </div>    
@@ -55,6 +69,7 @@ const populateMeals = async () => {
     btn.addEventListener('click', () => {
       document.querySelector(`#section-${btn.dataset.mealid}`).classList.remove('hide');
     });
+
     document.querySelectorAll('.comment-form').forEach((form) => {
       form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -73,7 +88,7 @@ const populateMeals = async () => {
   });
 
   displayLikes(likes);
-  // });
+  handleLike(document.querySelectorAll('.like'));
 };
 
 export default populateMeals;
