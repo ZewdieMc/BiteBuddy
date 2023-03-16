096502799753e360ec59ede731a27e09c88d6417
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-16 14:09:22 +0100

    Add displayComments function

--- src/modules/involvement.js
+++ src/modules/involvement.js
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase, no-console */
 
-const pushComment = (meal) => {
-  const appId = 'QKwdfW5YtFFU0z305ADd';
-  const involvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const appId = 'QKwdfW5YtFFU0z305ADd';
+const involvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 
+const pushComment = async (meal) => {
   const form = document.getElementById(`form-${meal.idCategory}`);
   const postComment = async (url, form) => {
     const formData = new FormData(form);
@@ -27,4 +27,24 @@ const pushComment = (meal) => {
   postComment(`${involvementUrl}apps/${appId}/comments`, form);
 };
 
-export default pushComment;
+const displayComments = async (meal) => {
+  const commentDisplay = document.getElementById(`display-${meal.idCategory}`);
+  const getComments = async (url) => {
+    const response = await fetch(url);
+    const commentArray = await response.json();
+    return commentArray;
+  };
+
+  const comments = await getComments(`${involvementUrl}apps/${appId}/comments?item_id=section-${meal.idCategory}`);
+
+  const commentsContainer = document.getElementById(`container-${meal.idCategory}`);
+  commentsContainer.innerHTML = '';
+
+  comments.forEach((element) => {
+    const comment = document.createElement('p');
+    comment.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
+    commentsContainer.appendChild(comment);
+  });
+  commentDisplay.firstChildElement.textContent = `Comments (${comments.length})`;
+};
+export { pushComment, displayComments };
